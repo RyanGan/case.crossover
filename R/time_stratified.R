ed289dace1b3933159c03db2ad06187cfba3453d
RyanGan <ryan.william.gan@gmail.com>
2018-04-16 13:14:44 -0400

    updated interior of function

--- R/time_stratified.R
+++ R/time_stratified.R
@@ -30,22 +30,19 @@ time_stratified <- function(data, id, covariate=F, admit_date,
   id_date <- data.frame(id_vec, admit_date)
 
   # create list of vectors of referent dates and admission date
-  referent_date_list <- apply(id_date, 1, function(x){
-    date_prior <- as.character(seq(as.Date(x[2]),as.Date(start_date),
+  referent_date_list <- apply(id_date, 1, function(x) {
+    date_prior <- as.character(seq(as.Date(x[2]), as.Date(start_date),
                                    by = (-interval)))
-    date_posterior <- as.character(seq(as.Date(x[2]),as.Date(end_date),
+    date_posterior <- as.character(seq(as.Date(x[2]), as.Date(end_date),
                                        by = (interval)))
-    date <- sort(unique(c(date_prior, date_posterior)))
+    date <- as.Date(sort(unique(c(date_prior, date_posterior))),
+                    format = "%Y-%m-%d")
     identifier <- rep(x[1], length(date))
-    # outcome
     outcome <- ifelse(date %in% x[2], 1, 0)
-    id_date_vec <- cbind(identifier, date, outcome)
-    return(id_date_vec)
+    id_date_vec <- data.frame(identifier, date, outcome)
   }) # end apply
   # timestrat dataframe
   ts_data <- do.call(rbind, referent_date_list)
-  rownames(ts_data) <- NULL
-  ts_data <- as.data.frame(ts_data)
   # remove white space
   ts_data$identifier <- gsub("[[:space:]]", "",
                              as.character(ts_data$identifier))
